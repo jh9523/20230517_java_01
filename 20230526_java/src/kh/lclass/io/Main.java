c70c64f98eba1ca2364c14cddc85c881719ed6fe
jh9523 <jhjhnft@gmail.com>
2023-06-05 17:01:13 +0900

    예외처리

--- 20230526_java/src/kh/lclass/io/Main.java
+++ 20230526_java/src/kh/lclass/io/Main.java
@@ -2,11 +2,27 @@ package kh.lclass.io;
 
 import java.io.IOException;
 
+import kh.lclass.exception.UserException;
+
 public class Main {
 
 	public static void main(String[] args) /*throws IOException*/{
 		// TODO Auto-generated method stub
-		new TestFileIO().testFile();
+//		new TestFileIO().testFile();
+//		new TestFileIO().testFileRead();
+//		new TestFileIO().testFileRead2();
+//		new TestFileIO().testFileRead3();
+//		try {
+//			new TestFileIO().testRamda();
+//		} catch (UserException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		new TestFileIO().testFileReadData();
+		new TestFileIO().testFileOutputStreamObject();
+		new TestFileIO().testFileInputStreamObject();
+		
+		System.out.println("=== main끝 ===");
 
 	}
 
--- 20230526_java/src/kh/lclass/oop/sample/Person.java
+++ 20230526_java/src/kh/lclass/oop/sample/Person.java
@@ -1,10 +1,24 @@
 package kh.lclass.oop.sample;
 
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Serializable {
 	
+	/**
+	 * 이 쪽 주석은 색이 다름 (보통은 초록 여기는 파랑)
+	 * java docs에 표시됨.
+	 * 여기에 UID 생성날짜, 누가, 무엇때문에 작성하였는지를 남겨야 함. 
+	 */
+	private static final long serialVersionUID = 5289587148873536011L;
 	private String name;	// 주민등록상 이름
-	private int age;
-	private char gender;
+	private int age;		// 만
+	private transient char gender;	// '남'	/	'여'
+	
+	
+	// 기본생성자
+	/*녹색*/
+	/***/ 			
+	/**여기는 파랑 작성위치는 여기*/
 	
 	public Person() {}
 	public  Person(String name, int age, char gender) {
