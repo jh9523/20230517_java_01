afcae97355e514df74097d7b7ebbaf1f1bf2980c
jh9523 <jhjhnft@gmail.com>
2023-06-09 17:33:27 +0900

    tcpserver

--- 20230609_tcp_server/src/kh/lclass/tcp/ServerMain.java
+++ 20230609_tcp_server/src/kh/lclass/tcp/ServerMain.java
@@ -3,8 +3,8 @@ package kh.lclass.tcp;
 public class ServerMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new TcpServer().testTcpServer(9001);	// 1. 서버의 포트번호 정함 ==> 프로그램의 포트번호
+		// 1. 서버의 포트번호 정함 ==> 프로그램의 포트번호
+		new TcpServer().testTcpServer(9001);	
 	}
 
 }
